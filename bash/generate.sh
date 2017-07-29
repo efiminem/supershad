02894f7d74580c1de2b34ff1ee267707582859b2
efiminem <efimmazhnik@gmail.com>
2017-07-29 22:18:43 +0300

    generation script

---
+++ bash/generate.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+dat=$1
+datt=$2
+for i in `seq 1 8`;
+        do
+                if [ -e ../source/$dat/$i/problem.mjax ]
+                then
+                	ALL+='<li>'`cat ../source/$dat/$i/problem.mjax`
+                	if [ -e ../source/$dat/$i/solution\ 1.mjax ]
+                	then
+                		ALL+="<div class=\"solution\" toid = \"text$i\">Решение</div>"
+                		ALL+="<div class=\"solution-text\" id=\"text$i\">"
+                		ALL+=`cat ../source/$dat/$i/solution\ 1.mjax`
+                		ALL+='</div>'
+                	fi
+                	for k in `seq 2 5`;
+                		do
+					if [ -e ../source/$dat/$i/solution\ $k.mjax ]
+                			then
+                				ALL+="<div class=\"solution\" toid = \"text$i\">Решение $k</div>"
+                				ALL+="<div class=\"solution-text\" id=\"text$i\">"
+                				ALL+=`cat ../source/$dat/$i/solution\ $k.mjax`
+                				ALL+='</div>'
+                			fi
+				done
+                	ALL+='</li>'
+		fi
+        done    
+IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$ALL")
+AllEscaped=${REPLY%$'\n'}
+sed "s/!PROBLEMS/$AllEscaped/;s/!DATA/$datt/" template.tmp > ../docs/$dat/indexr.html
+
