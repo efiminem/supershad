0c1bc70d9373831d765adecf82330f76d57a58e7
Unknown <Vliseev.92@mail.ru>
2018-04-08 21:22:24 +0300

    27-05-2017

--- gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-name=$1
-#cat $name
-#name="/home/vlad/Doc/supershad/source/20-05-2017/4/solution 1.tex"
-sed -r 's/([^$]|^)\${1}([^$]+)\${1}/\1<script type="math\/tex">\2<\/script>/g' "$name" | sed -r 's/([^$]|^)\${2}([^$]+)\${2}/<script type="math\/tex; mode=display">\2<\/script>/g'
